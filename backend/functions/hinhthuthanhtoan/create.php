d6e6e33943e95cfc214b1ed3da441f72d69416e1
leokaa <linhduy8a5@gmail.com>
2020-08-29 04:32:05 +0700

    29-sua

--- backend/functions/hinhthuthanhtoan/create.php
+++ backend/functions/hinhthuthanhtoan/create.php
@@ -25,30 +25,16 @@
                 <div class="col-md-4">
                 </div>
                 <div class="col-md-8">
-                <form name="frmThemMoi" id="frmThemMoi" method="post" action="">
-                    <table>
-                        <tr>
-                            <td><label for="exampleInputEmail1">Tên hình thức thanh toán</label></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" name="txt_httt_ten" id="txt_httt_ten" aria-describedby="emailHelp">
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <input type="submit" name="btnLuuDuLieu" id="btnLuuDuLieu" value="Lưu dữ liệu" />
-                                <a class="btn btn-outline-primary" href="index.php">Quay về danh sách</a>
-                            </td>
-                        </tr>
-                    </table>
-                </form>
+                    <h1>Tạo mới Hình thức Thanh toán</h1>
+                        <form name="frmHTTT" id="frmHTTT" method="post" action="">
+                        Tên hình thức thanh toán: <input type="text" name="httt_ten" id="httt_ten" />
+                        <br />
+                        <input type="submit" name="btnSave" id="btnSave" value="Lưu dữ liệu" />
+                        </form>
                     <?php
-                    if(isset($_POST['btnLuuDuLieu'])) {
+                    if(isset($_POST['btnSave'])) {
                         include_once(__DIR__ . '/../../../connect.php');
-                        $httt_ten = $_POST['txt_httt_ten']; 
+                        $httt_ten = $_POST['httt_ten']; 
                         $sql = "INSERT INTO `hinhthucthanhtoan`(httt_ten) VALUES('$httt_ten');";
                         mysqli_query($conn, $sql);
                     }
@@ -72,42 +58,62 @@
          include_once( __DIR__ . '/../../layouts/js.php');
     ?>
 
-     <script>
+    <script>
     $(document).ready(function() {
-         $("#frmThemMoi").validate({
-            rules: {
-                txt_httt_ten: {
-                    required: true,
-                    minlength: 3,
-                    maxlength: 50
-                }
-               },
-           messages: {
-                 txt_httt_ten: {
-                     required: "Vui lòng nhập Tên hình thức thanh toán",
-                     minlength: "Tên hình thức thanh toán phải có ít nhất 3 ký tự",
-                     maxlength: "Tên hình thức thanh toán không được vượt quá 50 ký tự"
-                 }
-             },
-             errorElement: "em",
-             errorPlacement: function(error, element) {
-                 // Thêm class `invalid-feedback` cho field đang có lỗi
-                 error.addClass("invalid-feedback");
-                 if (element.prop("type") === "checkbox") {
-                     error.insertAfter(element.parent("label"));
-                 } else {
-                     error.insertAfter(element);
-                 }
-             },
-             success: function(label, element) {},
-             highlight: function(element, errorClass, validClass) {
-                 $(element).addClass("is-invalid").removeClass("is-valid");
-             },
-             unhighlight: function(element, errorClass, validClass) {
-                 $(element).addClass("is-valid").removeClass("is-invalid");
-             }
-         });
+      $("#frThemMoi").validate({
+        rules: {
+          category_code: {
+            required: true,
+            minlength: 3,
+            maxlength: 50
+          },
+          category_name: {
+            required: true,
+            minlength: 3,
+            maxlength: 50
+          },
+          description: {
+            required: true,
+            minlength: 3,
+            maxlength: 255
+          }
+        },
+        messages: {
+          category_code: {
+            required: "Vui lòng nhập Mã Loại sản phẩm",
+            minlength: "Mã Loại sản phẩm phải có ít nhất 3 ký tự",
+            maxlength: "Mã Loại sản phẩm không được vượt quá 50 ký tự"
+          },
+          category_name: {
+            required: "Vui lòng nhập tên Loại sản phẩm",
+            minlength: "Tên Loại sản phẩm phải có ít nhất 3 ký tự",
+            maxlength: "Tên Loại sản phẩm không được vượt quá 50 ký tự"
+          },
+          description: {
+            required: "Vui lòng nhập mô tả cho Loại sản phẩm",
+            minlength: "Mô tả cho Loại sản phẩm phải có ít nhất 3 ký tự",
+            maxlength: "Mô tả cho Loại sản phẩm không được vượt quá 255 ký tự"
+          },
+        },
+        errorElement: "em",
+        errorPlacement: function(error, element) {
+          // Thêm class `invalid-feedback` cho field đang có lỗi
+          error.addClass("invalid-feedback");
+          if (element.prop("type") === "checkbox") {
+            error.insertAfter(element.parent("label"));
+          } else {
+            error.insertAfter(element);
+          }
+        },
+        success: function(label, element) {},
+        highlight: function(element, errorClass, validClass) {
+          $(element).addClass("is-invalid").removeClass("is-valid");
+        },
+        unhighlight: function(element, errorClass, validClass) {
+          $(element).addClass("is-valid").removeClass("is-invalid");
+        }
+      });
     });
-    </script>
+  </script>
 </body>
 </html>
